6fa43f95d25ac6cbe25a90d2f213c3232ed1798e
Aaron Miller <acmiller273@gmail.com>
2019-03-20 21:54:56 -0400

    Fix failure to load params in release mode with asserts disabled

--- include/ros_utils/ParamUtils.hpp
+++ include/ros_utils/ParamUtils.hpp
@@ -3,22 +3,27 @@
 
 #include <ros/ros.h>
 #include <string>
+#include <sstream>
 
 namespace ros_utils {
 
 class ParamUtils {
   public:
-    template<typename T>
-    static T getParam(const ros::NodeHandle& nh, const std::string& name)
-    {
+    template <typename T>
+    static T getParam(const ros::NodeHandle& nh, const std::string& name) {
         T val;
-        ROS_ASSERT_MSG(nh.getParam(name, val),
-                       "Failed to retrieve parameter: %s",
-                       name.c_str());
+        const bool success = nh.getParam(name, val);
+
+        if (!success) {
+            std::ostringstream ss;
+            ss << "Failed to retrieve parameter: " << name;
+            throw std::runtime_error(ss.str().c_str());
+        }
+
         return val;
     }
 };
 
-} // namespace ros_utils
+}  // namespace ros_utils
 
-#endif // include guard
+#endif  // include guard
